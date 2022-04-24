ba3c3ae6fed9587d014f310dd01979c0ed66e310
Kevin Chen <kc3585@nyu.edu>
2022-04-24 08:06:56 +0100

    add ray gun shooting sound effect

--- Assets/Code/JoyStickMovement/HapticsControl.cs
+++ Assets/Code/JoyStickMovement/HapticsControl.cs
@@ -8,11 +8,19 @@ public class HapticsControl : MonoBehaviour
     int bulletForce = 500;
     public GameObject bulletPrefab;
     public GameObject player;
+    AudioSource _audioSource;
+    public AudioClip raygunSound;
+
+    public void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     public void Vibrate() {
         GameObject newBullet = Instantiate(bulletPrefab, player.transform.position, Quaternion.identity);
         newBullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -(bulletForce * transform.localScale.y)));
         Destroy(newBullet, 0.5f);
+        _audioSource.PlayOneShot(raygunSound);
         Handheld.Vibrate();
     }
 }
