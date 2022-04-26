a998692afcdc1cbe226338138f04dd7bc31f387f
Kevin Chen <kc3585@nyu.edu>
2022-04-26 14:45:32 +0100

    working on debug minigame

--- Assets/Code/Room 1 Scene/DeBugMinigame.cs
+++ Assets/Code/Room 1 Scene/DeBugMinigame.cs
@@ -10,27 +10,62 @@ public class DeBugMinigame : MonoBehaviour
 
     RectTransform panel;
 
+    public Collider2D collider;
+
+
     // Start is called before the first frame update
     void Start()
     {
         panel = gameObject.GetComponent<RectTransform>();
-        createBug();
+        StartCoroutine(bugGame());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckForTouch();
+    }
+
+    IEnumerator bugGame()
+    {
+        int i;
+        for (i = 0; i < 10; i++) {
+            createBug();
+            yield return new WaitForSeconds(1);
+        }
+        yield return null;
     }
 
     void createBug() {
-        // Vector2 spawnPosition = new Vector2(Random.Range(0, panel.rect.x), Random.Range(0, panel.rect.y));
-        Vector2 spawnPosition = new Vector2(100, 100);
+        Vector2 spawnPosition = new Vector2(Random.Range(0, 1792), Random.Range(0, 828));
+        // Vector2 spawnPosition = new Vector2(1000, 500);
         print("Debug: " + spawnPosition);
-        GameObject newBug = Instantiate(bugPrefab, spawnPosition, Quaternion.identity) as GameObject;
-        // newBug.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -(bugMoveForce * transform.localScale.y)));
-        // newBug.transform.SetParent(gameObject.transform);
-        newBug.transform.SetParent (GameObject.FindGameObjectWithTag("Canvas").transform, false);
 
+        GameObject newBug = GameObject.Instantiate(bugPrefab, spawnPosition, Quaternion.identity, gameObject.transform);
+        // GameObject newBug = Instantiate(bugPrefab, spawnPosition, Quaternion.identity) as GameObject;
+        
+        newBug.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -(bugMoveForce * transform.localScale.y)));
+        
+        // newBug.transform.SetParent(gameObject.transform);
+        // newBug.transform.SetParent (GameObject.FindGameObjectWithTag("Canvas").transform, false);
     }
+
+    bool CheckForTouch()
+     {
+         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+         {
+            print(collider);
+             var wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+             var touchPosition = new Vector2(wp.x, wp.y);
+ 
+            print(touchPosition + " " + Physics2D.OverlapPoint(touchPosition));
+             if (collider == Physics2D.OverlapPoint(touchPosition)){
+                 Debug.Log("HIT!");
+             }
+             else{
+                  Debug.Log("MISS");
+             }
+         }
+         return false;
+     }
 }
