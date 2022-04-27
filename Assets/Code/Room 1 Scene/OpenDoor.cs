6bfe59cee7cfe18c7c2b70a2d7d13e19494165f6
Kevin Chen <kc3585@nyu.edu>
2022-04-27 21:17:34 +0100

    got door opening

--- Assets/Code/Room 1 Scene/OpenDoor.cs
+++ Assets/Code/Room 1 Scene/OpenDoor.cs
@@ -9,6 +9,9 @@ public class OpenDoor : MonoBehaviour
     //  private Collider2D collider;
     public GameObject lockedDoor;
     public GameObject unlockedDoor;
+    public GameObject leftDoor;
+
+    bool isDoorLocked = true;
 
     // Start is called before the first frame update
     void Start()
@@ -58,5 +61,24 @@ public class OpenDoor : MonoBehaviour
     public void UnlockDoor() {
         lockedDoor.SetActive(false);
         unlockedDoor.SetActive(true);
+        isDoorLocked = false;
+
+        if (!isDoorLocked) StartCoroutine(SlideDoor());
+    }
+
+    IEnumerator SlideDoor() {
+        float t = 0;
+        Vector3 startLeftDoorPosition = leftDoor.transform.position;
+        Vector3 startRightDoorPosition = unlockedDoor.transform.position;
+        Vector3 newLeftDoorPosition = startLeftDoorPosition + new Vector3(-1, 0, 0);
+        Vector3 newRightDoorPosition = startRightDoorPosition + new Vector3(1, 0, 0);
+
+        while (t < 10)
+        {
+            leftDoor.transform.position = Vector2.Lerp(startLeftDoorPosition, newLeftDoorPosition, t);
+            unlockedDoor.transform.position = Vector2.Lerp(startRightDoorPosition, newRightDoorPosition, t);
+            t += Time.deltaTime;
+            yield return null;
+        }
     }
 }
