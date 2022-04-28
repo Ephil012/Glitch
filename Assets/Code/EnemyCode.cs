370bcab0783eda9064732e87cb690ced2b2c92d8
jsoranno15 <99213201+jsoranno15@users.noreply.github.com>
2022-04-28 01:25:54 -0400

    added death animations

--- Assets/Code/EnemyCode.cs
+++ Assets/Code/EnemyCode.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class EnemyCode : MonoBehaviour
 {
+    Animator _animator;
+    Animation _animation;
 
     int health = 100;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
+        _animation = gameObject.GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (health <= 0) {
-            Destroy(gameObject);
+            _animator.SetTrigger("Death");
+            Destroy(gameObject , 0.64f);
+           
         }
     }
-
+    
     public void DamageEnemy() {
         print("EnemyCode: DamageEnemy - Received Broadcast");
         health -= 20;
