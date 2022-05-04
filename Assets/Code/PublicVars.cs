9bc4e98ea9a4c68832223e21346e06583ed10c1f
Kevin Chen <kc3585@nyu.edu>
2022-05-04 17:09:36 +0100

    fix canvas prefab + add heart to canvas

---
+++ Assets/Code/PublicVars.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PublicVars : MonoBehaviour
+{
+
+    public static int health = 100;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
