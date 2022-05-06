320b010d958cdcd009f04e75ce0e5d30402b4c49
dominique <dominiquemcc3@gmail.com>
2022-05-06 18:44:06 -0400

    FixedPuzzles

--- Assets/Code/Puzzle/DropSlot.cs
+++ Assets/Code/Puzzle/DropSlot.cs
@@ -11,7 +11,16 @@ public class DropSlot : MonoBehaviour, IDropHandler
     private bool pointCheck = false;
 
     private GameObject slotObject;
+    private bool tagChanged = false;
 
+    public void Start()
+    {
+        if (actualTag.Contains("Straight") && !actualTag.Contains("plus") && !actualTag.Contains("minus"))
+        {
+            actualTag = "Straight";
+            tagChanged = true;
+        }
+    }
     public void OnDrop(PointerEventData eventData)
     {
         if (eventData.pointerDrag != null)
@@ -28,12 +37,27 @@ public class DropSlot : MonoBehaviour, IDropHandler
             slotObject.transform.Rotate(0, 0, -90);
         }
 
-        //correct piece inserted
-        if (slotObject != null && slotObject.GetComponent<RectTransform>().rotation.eulerAngles.z == actualRotation && slotObject.tag == actualTag && !pointCheck)
+        if (tagChanged)
         {
-            pointCheck = true;
-            puzzleControl.GetComponent<PuzzleInteract>().points += 1;
-            Debug.Log("Correct Rotation");
+            //correct piece inserted
+            if (slotObject != null && slotObject.GetComponent<RectTransform>().rotation.eulerAngles.z == actualRotation && (slotObject.tag.Contains(actualTag) && !slotObject.tag.Contains("minus") && !slotObject.tag.Contains("plus")) && !pointCheck)
+            {
+                pointCheck = true;
+                puzzleControl.GetComponent<PuzzleInteract>().points += 1;
+                Debug.Log("Correct Rotation");
+            }
         }
+        else
+        {
+            //correct piece inserted
+            if (slotObject != null && slotObject.GetComponent<RectTransform>().rotation.eulerAngles.z == actualRotation && slotObject.CompareTag(actualTag) && !pointCheck)
+            {
+                pointCheck = true;
+                puzzleControl.GetComponent<PuzzleInteract>().points += 1;
+                Debug.Log("Correct Rotation");
+            }
+        }
+
+        
     }
 }
